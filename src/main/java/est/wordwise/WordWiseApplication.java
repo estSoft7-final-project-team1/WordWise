8cc7cc12864e31659ff721b0cfb9b9721b3a329f
myh7754 <myh4755@gmail.com>
2025-01-10 19:33:36 +0900

    first commit

---
+++ src/main/java/est/wordwise/WordWiseApplication.java
@@ -0,0 +1,13 @@
+package est.wordwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/est/wordwise/WordWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package est.wordwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
