7134c753cf2d1e337ee11e0edce96f81413e8eb6
wgrgwg <kyoeng14697@naver.com>
2025-01-15 14:39:35 +0900

    test: word create, read 테스트 추가

--- src/test/java/est/wordwise/domain/word/service/WordServiceTest.java
+++ src/test/java/est/wordwise/domain/word/service/WordServiceTest.java
@@ -1,29 +1,55 @@
 package est.wordwise.domain.word.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import est.wordwise.common.entity.Word;
+import est.wordwise.domain.example.dto.ExampleCreateDto;
+import est.wordwise.domain.word.dto.WordCreateDto;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @SpringBootTest
+@Transactional
 class WordServiceTest {
 
     @Autowired
     private WordService wordService;
+    private WordCreateDto wordCreateDto;
+    private ExampleCreateDto exampleCreateDto;
 
-    @Test
-    void test() {
-        String temp = "";
-        System.out.println(temp);
+    @BeforeEach
+    void setUp() {
+        wordCreateDto = WordCreateDto.of("general", "일반적인,대체적인,장군");
+        exampleCreateDto = ExampleCreateDto.builder()
+            .sentence("This is a general overview of the project.")
+            .sentenceMeaning("이것은 프로젝트의 일반적인 개요입니다.")
+            .build();
     }
 
     @Test
-    @DisplayName("RestClient 간단한 요청-응답 테스트")
-    void alanApiTest() {
+    @Rollback
+    @DisplayName("Word create 테스트")
+    void createWordTest() {
+        Word findWord = wordService.createWord(wordCreateDto);
 
-        String question = "Yes라고 대답해줘";
-        
+        assertThat(findWord.getWordText()).isEqualTo(wordCreateDto.getWordText());
+        assertThat(findWord.getDefinition()).isEqualTo(wordCreateDto.getDefinition());
+    }
+
+    @Test
+    @Rollback
+    @DisplayName("Word read 테스트")
+    void readWordTest() {
+        wordService.createWord(wordCreateDto);
+        Word findWord = wordService.getWordByWordText(wordCreateDto.getWordText());
+
+        assertThat(findWord.getDefinition()).isEqualTo(wordCreateDto.getDefinition());
     }
 }
