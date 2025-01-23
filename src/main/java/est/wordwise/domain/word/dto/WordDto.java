192f2545e780909901b4f9c80de306188bd9deed
wgrgwg <kyoeng14697@naver.com>
2025-01-23 15:06:58 +0900

    feat: 이전 출력 예문 기억 로직 추가

--- src/main/java/est/wordwise/domain/word/dto/WordDto.java
+++ src/main/java/est/wordwise/domain/word/dto/WordDto.java
@@ -1,8 +1,10 @@
 package est.wordwise.domain.word.dto;
 
+import est.wordwise.common.entity.Example;
 import est.wordwise.common.entity.Word;
 import est.wordwise.domain.alanapi.dto.ResponseContent;
 import est.wordwise.domain.example.dto.ExampleDto;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
@@ -14,13 +16,32 @@ public class WordDto {
     private String wordText;
     private String definition;
     List<ExampleDto> exampleDtos;
+    @Builder.Default
+    List<Long> formerExampleIds = new ArrayList<>();
 
-    public static WordDto from(Word word) {
-        return WordDto.builder()
+    public static WordDto from(Word word, List<Example> examples) {
+        WordDto wordDto = WordDto.builder()
             .wordText(word.getWordText())
             .definition(word.getDefinition())
-            .exampleDtos(word.getExamples().stream().map(ExampleDto::from).toList())
+            .exampleDtos(examples.stream().map(ExampleDto::from).toList())
             .build();
+
+        wordDto.getFormerExampleIds().addAll(examples.stream().map(Example::getId).toList());
+
+        return wordDto;
+    }
+
+    public static WordDto from(Word word, List<Example> examples, WordDto formerWordDto) {
+        WordDto wordDto = WordDto.builder()
+            .wordText(word.getWordText())
+            .definition(word.getDefinition())
+            .exampleDtos(examples.stream().map(ExampleDto::from).toList())
+            .formerExampleIds(formerWordDto.getFormerExampleIds())
+            .build();
+
+        wordDto.getFormerExampleIds().addAll(examples.stream().map(Example::getId).toList());
+
+        return wordDto;
     }
 
     public static WordDto from(ResponseContent responseContent) {
